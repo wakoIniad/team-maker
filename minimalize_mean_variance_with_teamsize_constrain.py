cc8b160ae309c1d4dd1fee883cec80b994e68af1
wakoIniad <168269931+wakoIniad@users.noreply.github.com>
2026-05-27 19:28:31 +0900

    使いやすいように少し改良

--- minimalize_mean_variance_with_teamsize_constrain.py
+++ minimalize_mean_variance_with_teamsize_constrain.py
@@ -3,7 +3,7 @@ import math
 import random
 
 #ここにメンバー数と、基本のチームサイズを入力(余る場合はBASE_TEAM_SIZE+1人班ができます)
-MEMBER_COUNT = 100
+MEMBER_COUNT = 16
 BASE_TEAM_SIZE = 4
 
 ADJUST_TEAM_COUNT = MEMBER_COUNT % BASE_TEAM_SIZE
@@ -14,9 +14,36 @@ M, N = MEMBER_COUNT, TEAM_COUNT
 #元のデータ上でパラメーターが複数ある場合は重みをかけて足し合わせる
 #[重要] 後半でやってる酷いコードの影響で、パラメータの値が小さい人がある程度発生するように調整するとうまくいきやすいかも(1とか2くらい)
 #(よくない設定でも動くことを確かめるために１が少なくなりやすい設定で試している -> 大体うまく動いた)
-parametorArray = [math.ceil(random.randint(1,16)**0.5) for _ in range(MEMBER_COUNT)] 
+
+#SAMPLE
+input_data = {
+    "NAME1": { "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME2": { "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME3": { "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME4": { "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME5": { "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME6": { "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME7": { "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME8": { "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME9": { "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME10":{ "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME11":{ "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME12":{ "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME13":{ "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME14":{ "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME15":{ "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) },
+    "NAME16":{ "param1" : math.ceil(random.randint(1,16)**0.5), "param2": math.ceil(random.randint(1,16)**0.5) }
+}
+
+### ※input_dataのkeysに順序の保証があることを前提 ないならどうにかして ###
+MEMBER_NAME_TAG = [*input_data.keys()]
+
+### これ設定する ###
+GetSummaryParam = lambda params: params["param1"] * 0.9 + params["param1"] * 0.1
 
 
+#parametorArray = [math.ceil(random.randint(1,16)**0.5) for _ in range(MEMBER_COUNT)] 
+parametorArray = [GetSummaryParam(value) for value in input_data.values()]
 x = [[pulp.LpVariable(f"x_{i}_{j}", cat="Binary") for j in range(N)] for i in range(M)]
 
 z = [pulp.LpVariable(f"z_{i}", lowBound=0) for i in range(N)]
@@ -52,10 +79,12 @@ for j in range(N):
     print(f"team[{j}] info:")
     print(f"\tcount {sum(pulp.value(x[i][j]) for i in range(M))}")
     print(f"\tability {sum(pulp.value(x[i][j]) * parametorArray[i] for i in range(M))}")
-    print(f"\tmembers {",".join(filter(lambda mem: mem != "no", [str(i) if pulp.value(x[i][j]) else str("no") for i in range(M)]))}")
+    
+    print(f"\tmembers {",".join(filter(lambda mem: mem != "no", [str(MEMBER_NAME_TAG[i]) if pulp.value(x[i][j]) else str("no") for i in range(M)]))}")
 
 
 #ここがFalseだと、誰かが複数のチームに割り当てられてしまっている
 print(f"\n{noDuplicate=}")
 #最適化の際に目標にしている値
 print(f"idealAbility = {u}")
+
